ce447aa64b36610a6c665b7370c2a3d866f6ad5b
wenhaox <111180600+wenhao-nus@users.noreply.github.com>
2026-01-09 01:34:38 +0800

    dummy events

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
+import events from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
     'use cache';
     cacheLife('hours')
-    const response = await fetch(`${BASE_URL}/api/events`);
-    const { events } = await response.json();
+    // const response = await fetch(`${BASE_URL}/api/events`);
+    // const { events } = await response.json();
 
     return (
         <section>
